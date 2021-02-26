ef70ce3ea60071f6de08151d51852c451eeeb0f6
MohammadR-M <mohammadrezamirzadejava@gmail.com>
2021-02-26 18:51:03 +0330

    Fix exe8.

--- exe8/routes/signup.js
+++ exe8/routes/signup.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get("/", function (req, res){
     res.render("./pages/signup");
@@ -7,11 +9,21 @@ router.get("/", function (req, res){
 
 router.post("/", function (req, res){
     fs.readFile(path.join(__dirname, "../public/data/users.json"), 'utf8', function (err, data){
-        let user = JSON.parse(data).find(function (element){
+        let users = JSON.parse(data);
+        let user = users.find(function (element){
             return element.user === req.body.user;
         });
         if (!user){
-            fs.wr
+            user = {
+                user: req.body.user,
+                password: req.body.password,
+                email: req.body.email,
+                isLoggedIn: false
+            }
+            users.push(user);
+            fs.writeFile(path.join(__dirname, "../public/data/users.json"), JSON.stringify(users), function (err){
+                if (err) return console.log(err);
+            });
             res.redirect("/login");
         } else {
             res.send("user");
